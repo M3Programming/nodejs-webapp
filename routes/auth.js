ec37716150f14bffa9e16d8dd098e2fb985714f8
M3 <115448632+M3Programming@users.noreply.github.com>
2024-06-10 14:32:17 +0800

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -7,81 +7,127 @@ const parseUrl = require('body-parser');
 let encodeUrl = parseUrl.urlencoded({ extended: false });
 
 router.get('/register', function (req, res, next) {
-    res.render('register',{
-       
-            title: 'Registration Page',
-            name: '',
-            email: '',
-            password: ''     
-        
+    res.render('register', {
+
+        title: 'Registration Page',
+        email: '',
+        firstName: '',
+        lastName: '',
+        userName: '',
+        password: ''
+
     })
-})
+});
 
 router.post('/register', encodeUrl, (req, res) => {
 
-      //No errors were found.  Passed Validation!
-        let firstName = req.body.firstName;
-        let lastName = req.body.lastName;
-        let email = req.body.email;
-        let userName = req.body.userName;
-        let password = req.body.password;
+    //No errors were found.  Passed Validation!
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let email = req.body.email;
+    let userName = req.body.userName;
+    let password = req.body.password;
 
-        conn.connect(function (err) {
-            if (err) {
-                console.log(err);
-            };
-            // checking user already registered or no
-           conn.query(`SELECT * FROM user_details WHERE username = '${userName}' AND password  = '${password}'`, function (err, result) {
+
+    if (err) {
+        console.log(err);
+    };
+    // checking user already registered or not
+    conn.query(`SELECT * FROM user_details WHERE username = '${userName}' AND password  = '${password}'`, function (err, result) {
+        if (err) {
+            console.log(err);
+        };
+        if (Object.keys(result).length > 0) {
+            res.sendFile(__dirname + '/failreg.html');
+        } else {
+            // inserting new user data
+            let sql = `INSERT INTO user_details (email_add, first_name,last_name, username, password) VALUES ('${email}', '${firstName}','${lastName}', '${userName}', '${password}')`;
+            conn.query(sql, function (err, result) {
                 if (err) {
-                    console.log(err);
-                };
-                if (Object.keys(result).length > 0) {
-                    res.sendFile(__dirname + '/failreg.html');
+                    req.flash('error', err)
+
+                    res.render('auth/register', {
+                        title: 'Registration Page',
+                        email: '',
+                        firstName: '',
+                        lastName: '',
+                        userName: '',
+                        password: ''
+                    })
                 } else {
-                    //creating user page in userPage function
-                    function userPage() {
-                        // We create a session for the dashboard (user page) page and save the user data to this session:
-                        req.session.user = {
-                            firstname: firstName,
-                            lastname: lastName,
-                            emailadd: email,
-                            username: userName,
-                            userpassword: password
-                        };
-
-                        res.send(`
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <title>Login and register form with Node.js, Express.js and MySQL</title>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-                </head>
-                <body>
-                    <div class="container">
-                        <h3>Hi, ${req.session.user.firstname} ${req.session.user.lastname}</h3>
-                        <a href="/">Log out</a>
-                    </div>
-                </body>
-                </html>
-                `);
-                    }
-                    // inserting new user data
-                    let sql = `INSERT INTO user_details (email_add, first_name,last_name, username, password) VALUES ('${email}', '${firstName}','${lastName}', '${userName}', '${password}')`;
-                    conn.query(sql, function (err, result) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            // using userPage function for creating user page
-                            userPage();
-                        };
-                    });
-
-                }
-
+                    req.flash('success', 'You have successfully signup!');
+                    res.redirect('/login');
+                    
+                                };
             });
-        });
+
+        }
+
+    });
+
 });
+
+//display login page
+router.get('/login', function(req, res,next){    
+    res.render('login', {
+        title: 'Login',
+        userName: '',
+        password: ''     
+    })
+});
+
+//authenticate user
+router.post('/authentication', function(req, res,next) {
+      
+    let user_name = req.body.userName;
+    let user_password = req.body.password;
+    if (username && password) {
+        connection.query('SELECT * FROM user_details WHERE username = ? AND password = ?', [user_name, user_password], function(err, rows, fields) {
+            if(err) throw err;
+            
+            // if user not found
+            if (rows.length <= 0) {
+                req.flash('error', 'Please enter the correct username and Password!')
+                res.redirect('/login')
+            }
+            else { // if user found
+                // render to views/user/edit.ejs template file
+                req.session.loggedin = true;
+                req.session.name = rows[0].name;
+                res.redirect('/home');
+
+            }   
+                     
+       
+        })
+    }else {
+            response.send('Please enter Username and Password!');
+            response.end();
+        }
+ 
+});
+
+router.get('/home', function(req, res, next) {
+    if (req.session.loggedin) {
+        
+        res.render('auth/home', {
+            title:"Dashboard",
+            name: req.session.name,     
+        });
+
+    } else {
+
+        req.flash('success', 'Please login first!');
+        res.redirect('/login');
+    }
+});
+
+// Logout user
+router.get('/logout', function (req, res) {
+  req.session.destroy();
+  res.clearCookie('session');
+  res.redirect('/login');
+});
+
 
 module.exports = router;
